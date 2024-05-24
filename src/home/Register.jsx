e97d881f9854598ee63456c6106723b5df63723b
Rashad Aliyev <eliyevresad901@gmail.com>
2024-05-24 12:37:14 +0400

    register section is done

--- src/home/Register.jsx
+++ src/home/Register.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const subTitle = "Save The Day";
 const title = (
-  <h2>
+  <h2 className="title">
     Join on Day Long Free workshop for
     <b>
       Advance <span>Mastering</span>
@@ -14,7 +14,50 @@ const title = (
 const desc = "Limited Time Offer! ";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="register-section padding-tb pb-0">
+      <div className="container">
+        <div className="row g-4 row-cols-lg-2 row-cols-1 align-items-center ">
+          <div className="col">
+            <div className="section-header">
+              <span className="subtitle">{subTitle}</span>
+              {title}
+              <p>{desc}</p>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="section-wrapper">
+              <h4>Register Now</h4>
+              <form className="register-form">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="username"
+                  className="reg-input"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="reg-input"
+                />
+                <input
+                  type="number"
+                  name="number"
+                  placeholder="Phone"
+                  className="reg-input"
+                />
+                <button type="submit" className="lab-btn">
+                  Register Now
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
