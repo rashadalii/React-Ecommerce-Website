06a2a9808549ff184823fc55480bb862307272d4
Rashad Aliyev <eliyevresad901@gmail.com>
2024-05-24 14:29:33 +0400

    about section is ready

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,6 +4,7 @@ import HomeCategory from "./HomeCategory";
 import CategoryshowCase from "./CategoryshowCase";
 import Register from "./Register";
 import Location from "./Location";
+import AboutUs from "./AboutUs";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <CategoryshowCase />
       <Register />
       <Location />
+      <AboutUs />
     </>
   );
 };
