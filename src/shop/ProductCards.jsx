77c1c19ffe1d13064e736d850c6b2473c317c563
Rashad Aliyev <eliyevresad901@gmail.com>
2024-05-25 11:09:09 +0400

    all product is done

--- src/shop/ProductCards.jsx
+++ src/shop/ProductCards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
 
 const ProductCards = ({ GridList, products }) => {
   return (
@@ -29,6 +30,52 @@ const ProductCards = ({ GridList, products }) => {
                   <i className="icofont-cart-alt"></i>
                 </Link>
               </div>
+            </div>
+
+            {/* product content */}
+            <div className="product-content">
+              <h5>
+                <Link to={`/shop/${product.id}`}>{product.name}</Link>
+              </h5>
+              <p className="productRating">
+                <Rating />
+              </p>
+              <h6>${product.price}</h6>
+            </div>
+          </div>
+
+          {/* list style */}
+
+          <div className="product-list-item">
+            {/* product images */}
+            <div className="product-thumb">
+              <div className="pro-thumb">
+                <img src={product.img} alt="" />
+              </div>
+
+              {/* product action links */}
+              <div className="product-action-link">
+                <Link to={`/shop/${product.id}`}>
+                  <i className="icofont-eye"></i>
+                </Link>
+                <a href="">
+                  <i className="icofont-heart"></i>
+                </a>
+                <Link to="/cart-page">
+                  <i className="icofont-cart-alt"></i>
+                </Link>
+              </div>
+            </div>
+
+            {/* product content */}
+            <div className="product-content">
+              <h5>
+                <Link to={`/shop/${product.id}`}>{product.name}</Link>
+              </h5>
+              <p className="productRating">
+                <Rating />
+              </p>
+              <h6>${product.price}</h6>
             </div>
           </div>
         </div>
