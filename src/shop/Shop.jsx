b671a46f2554fea435d0d8217be9ad5be656b14e
Rashad Aliyev <eliyevresad901@gmail.com>
2024-05-25 12:53:00 +0400

    pagination is ready

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -4,10 +4,29 @@ import PageHeader from "../components/PageHeader";
 const showResults = "Showing 01-12 of 139 Results";
 import Data from "../products.json";
 import ProductCards from "./ProductCards";
+import Pagination from "./Pagination";
 
 const Shop = () => {
   const [GridList, setGridList] = useState(true);
   const [products, setProducts] = useState(Data);
+  console.log(products);
+
+  //   Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12;
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOdFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOdFirstProduct,
+    indexOfLastProduct
+  );
+
+  //   function to change the current page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       <PageHeader title="Our shop Page" curPage="Shop" />
@@ -33,10 +52,19 @@ const Shop = () => {
                     </a>
                   </div>
                 </div>
-
+                {/* product cards */}
                 <div>
-                  <ProductCards GridList={GridList} products={products} />
+                  <ProductCards
+                    GridList={GridList}
+                    products={currentProducts}
+                  />
                 </div>
+                <Pagination
+                  productsPerPage={productsPerPage}
+                  totalProducts={products.length}
+                  paginate={paginate}
+                  activePage={currentPage}
+                />
               </article>
             </div>
             <div className="col-lg-4 col-12"></div>
